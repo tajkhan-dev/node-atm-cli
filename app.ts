e34da48bcb0d913eb726cdc0357785d1be5ae1b1
Zainulabdeen-101 <bbadshah691@gmail.com>
2023-08-13 14:14:51 +0500

    setup completed

---
+++ app.ts
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+
+const Atm=async()=>{
+    const {pin} =await inquirer.prompt([
+        {
+            type:'number',
+            name:'pin',
+            message:'Enter Your Pin'
+        }
+    ])
+    console.log(pin)
+}
+
+Atm()
